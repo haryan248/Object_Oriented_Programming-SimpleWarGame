2ec6f9af740cd2d9f9472e580cc1d6af74dcbe67
haryan248 <51049245+haryan248@users.noreply.github.com>
2019-12-27 12:52:12 +0900

    Create King.java

---
+++ SimpleWarGame/src/Unit/King.java
@@ -0,0 +1,13 @@
+package Unit;
+
+public class King extends Unit {
+
+	public King(int x, int y) {
+		super(x, y);
+		unit_name = "K";
+		Hp = 15;
+		Attack = 0;
+		Move_range= 1;
+		Attack_range= 1;
+	}
+}
