214046178e8a85b9b527ba805afd7f05af481b2e
haryan248 <51049245+haryan248@users.noreply.github.com>
2019-12-27 12:52:03 +0900

    Create Archer.java

---
+++ SimpleWarGame/src/Unit/Archer.java
@@ -0,0 +1,47 @@
+package Unit;
+
+public class Archer extends Unit {
+	public Archer(int x, int y) {
+		super(x, y);
+		unit_name = "A";
+		Hp = 12;
+		Attack = 3;
+		Move_range = 2;
+		Attack_range = 4;
+		// 이동 거리 : 2칸/1턴
+		// 이동 방향 : 4방향(동서남북)
+		// 공격 범위 : 4칸
+		// 공격 방향 : 4방향
+
+	}
+
+	@Override // 대각선 이동불가능
+	public boolean MoveRangeCheck(int x, int y) {
+		if (Math.abs(x) <= Move_range && Math.abs(y) <= Move_range) {
+			if (x == 0 || y == 0)
+				return true;
+			else {
+				System.out.print("This unit can't move diagonal direction\n");
+				return false;
+			}
+		} else {
+			System.out.print("Exceed Unit move range\n");
+			return false;
+		}
+	}
+
+	@Override
+	public boolean AttackRangeCheck(int x, int y) {
+		if (Math.abs(x) <= Attack_range && Math.abs(y) <= Attack_range) {
+			if (x == 0 || y == 0)
+				return true;
+			else {
+				System.out.print("This unit can't attack diagonal direction\n");
+				return false;
+			}
+		} else {
+			System.out.println("Out of Range");
+			return false;
+		}
+	}
+}
